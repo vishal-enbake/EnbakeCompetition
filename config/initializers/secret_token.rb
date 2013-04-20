d09f613ae91ef2ace7524b07095e4cf1abb7aace
mukul <mugoyal@enbake.com>
2013-04-20 10:34:22 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnbakeCompetition::Application.config.secret_token = '989cf4d5e58d6bfac22034da1f66be4ab4812caad27ab62e81f0864c5e79038577ea5a0158f5869a6ac0c2ad6d9997db0a5c44753a477e313ae914b1a3cda2ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EnbakeCompetition::Application.config.session_store :cookie_store, key: '_EnbakeCompetition_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EnbakeCompetition::Application.config.session_store :active_record_store
